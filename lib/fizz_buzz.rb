fce55d7022be7a0d82d9fd561eac2696c2d6262b
DavDan1 <david-danielyan@hotmail.comm>
2021-03-17 19:12:10 +0100

    added a test that implements fizz when written 3

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,8 @@
 def fizz_buzz(number)
-number 
+if number %3==0
+    'fizz'
+    
+else
+    number
+end
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,9 +1,11 @@
 require './lib/fizz_buzz'
 
 describe "fizz_buzz" do
-    
     it 'returns 1 if number is 1' do
-        expect(fizz_buzz(1)).to eq 1
+        expect(fizz_buzz(1)).to eq 1 
+    end
+    it "returns 'fizz' if number is 3" do
+        expect(fizz_buzz(3)).to eq 'fizz'
     end
 
 
