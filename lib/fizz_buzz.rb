c55f8e8d33631956cd6060fb7fa44dd7847ee2f5
DavDan1 <david-danielyan@hotmail.comm>
2021-03-17 18:30:28 +0100

    writing first test to test that a nummber will be returned when a number is entered and creating method for fizz_buzz

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,3 @@
+def fizz_buzz(number)
+number 
+end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -2,4 +2,9 @@ require './lib/fizz_buzz'
 
 describe "fizz_buzz" do
     
+    it 'returns 1 if number is 1' do
+        expect(fizz_buzz(1)).to eq 1
+    end
+
+
 end
